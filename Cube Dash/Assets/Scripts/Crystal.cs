dda38219d097b9a8cba2f1dd153f174150f7d992
SeborExe <mix98@o2.pl>
2022-01-09 20:30:07 +0100

    Crystal and Crystal Counter
    Crystal object and crystal counter has created and work correct.

---
+++ Cube Dash/Assets/Scripts/Crystal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    public bool Active = true;
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(45f, Time.timeSinceLevelLoad * 60f, 45f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "Player" || !Active) return;
+
+        var renderer = GetComponent<Renderer>();
+        renderer.enabled = false;
+        Active =  false;
+        FindObjectOfType<CrystalCounter>().UpdateCrystalCounterText();
+    }
+}
---
+++ Cube Dash/Assets/Scripts/CrystalCounter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrystalCounter : MonoBehaviour
+{
+    public Text CrystalCounterText;
+    void Start()
+    {
+        UpdateCrystalCounterText();
+    }
+
+    public void UpdateCrystalCounterText()
+    {
+        var crystals = FindObjectsOfType<Crystal>();
+        var crystalCount = crystals.Length;
+        var crystalInactive = crystals.Count(crystal => !crystal.Active);
+
+        var text = crystalInactive + "/ " + crystalCount;
+        CrystalCounterText.text = text;
+    }
+}
