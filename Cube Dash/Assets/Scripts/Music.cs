b0d22eca690f7c4f5e0d2e385c972301bfcae2b5
SeborExe <mix98@o2.pl>
2022-01-10 09:45:51 +0100

    Options Basics
    Add music and slider who enables the player manipulate sound volume.

---
+++ Cube Dash/Assets/Scripts/Music.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Music : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource audioSource;
+    void Start()
+    {
+        FindObjectOfType<Music>().GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("musicVolume", 0.8f);
+
+        if (FindObjectsOfType<Music>().Length > 1)
+            Destroy(gameObject);
+
+        audioSource = FindObjectOfType<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[Random.Range(0, clips.Length)];
+    }
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = GetRandomClip();
+            audioSource.Play();
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
