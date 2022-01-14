0a6eb8b38b1073fc352e298fc0436be09770a417
SeborExe <mix98@o2.pl>
2022-01-14 12:39:28 +0100

    Shop Page and Scripts
    Shop page and all required scripts has been created.

---
+++ Cube Dash/Assets/Scripts/Shop/SkinShopItem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinShopItem : MonoBehaviour
+{
+    [SerializeField] private SkinManager skinManager;
+    [SerializeField] private int skinIndex;
+    [SerializeField] private Button buyButton;
+    [SerializeField] private Text costText;
+
+    private Skin skin;
+    void Start()
+    {
+        skin = skinManager.skins[skinIndex];
+
+        GetComponent<MeshRenderer>().material = skin.material;
+
+        if(skinManager.IsUnlocked(skinIndex))
+        {
+            buyButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            buyButton.gameObject.SetActive(true);
+            costText.text = skin.cost.ToString();
+        }
+    }
+
+    public void OnSkinPressed()
+    {
+        if (skinManager.IsUnlocked(skinIndex))
+        {
+            skinManager.SelectSkin(skinIndex);
+        }
+    }
+
+    public void OnBuyButtonPressed()
+    {
+        int coins = PlayerPrefs.GetInt("money", 0);
+
+        if(coins >= skin.cost && skinManager.IsUnlocked(skinIndex))
+        {
+            PlayerPrefs.SetInt("money", coins - skin.cost);
+            skinManager.Unlock(skinIndex);
+            buyButton.gameObject.SetActive(false);
+            skinManager.SelectSkin(skinIndex);
+        }
+    }
+}
