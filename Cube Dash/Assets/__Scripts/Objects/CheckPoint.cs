3d851fe8de486c3723c8d7e4062425ed7f0b0344
SeborExe <mix98@o2.pl>
2022-04-27 20:23:21 +0200

    Balance
    Now obstacles in levels are enebled by checkpoint. Game work more more clear!

--- Cube Dash/Assets/__Scripts/Objects/CheckPoint.cs
+++ Cube Dash/Assets/__Scripts/Objects/CheckPoint.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CheckPoint : MonoBehaviour
 {
+    public UnityEvent disableBlocks;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag != "Player") return;
 
         float pos = transform.position.z;
+        disableBlocks?.Invoke();
 
         CheckPointsStats.SetCurrentCheckPoint(pos);
     }
